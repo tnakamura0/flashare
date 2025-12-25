d0af20577fde567730bc726724203968013a182b
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2025-12-26 07:31:08 +0900

    update:cardsテーブルを作成するマイグレーションの適用

--- db/migrate/20251225222206_create_cards.rb
+++ db/migrate/20251225222206_create_cards.rb
@@ -1,8 +1,9 @@
 class CreateCards < ActiveRecord::Migration[7.2]
   def change
     create_table :cards do |t|
-      t.string :name
-      t.text :meaning
+      t.string :name, null: false
+      t.text :meaning, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_24_072015) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_25_222206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "meaning", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_24_072015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "cards", "users"
 end

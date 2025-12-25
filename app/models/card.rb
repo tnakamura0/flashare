442a5257a2ab10dc7f674c2eb29cf633fdd4ce02
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2025-12-26 07:38:30 +0900

    add:UserモデルとCardモデル間に関連付けを追加

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,6 @@
 class Card < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :meaning, presence: true, length: { maximum: 65_535 }
+
+  belongs_to :user
 end

e45a644cf418a9097f514bb772b2f5c2c633f1b8
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2025-12-28 07:26:12 +0900

    add:Deckのモデルスペックのテストの実装

--- spec/factories/decks.rb
+++ spec/factories/decks.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :deck do
-    name { "MyString" }
-    description { "MyText" }
+    name { Faker::Name.name }
+    description { Faker::Esport.game }
     public { false }
-    user { nil }
+    association :user
   end
 end
--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
+include SystemHelper
+
 RSpec.describe Deck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    it "全てのフィールドが有効な場合に有効であること" do
+      deck = build(:deck)
+      expect(deck).to be_valid
+    end
+
+    it "デッキ名が50文字以下の場合に有効であること" do
+      deck = build(:deck, name: "a" * 50)
+      expect(deck).to be_valid
+    end
+
+    it "説明が存在しない場合に有効であること" do
+      deck = build(:deck, description: nil)
+      expect(deck).to be_valid
+    end
+
+    it "説明が100文字以内の場合に有効であること" do
+      deck = build(:deck, description: "a" * 100)
+      expect(deck).to be_valid
+    end
+
+    it "公開設定がtrueの場合に有効であること" do
+      deck = build(:deck, public: true)
+      expect(deck).to be_valid
+      expect(deck.public).to eq true
+    end
+
+    it "公開設定がfalseの場合に有効であること" do
+      deck = build(:deck, public: false)
+      expect(deck).to be_valid
+      expect(deck.public).to eq false
+    end
+  end
+
+  describe "異常系" do
+    it "デッキ名が存在しない場合に無効であること" do
+      deck = build(:deck, name: nil)
+      expect(deck).to be_invalid
+      expect(deck.errors[:name]).to include("を入力してください")
+    end
+
+    it "デッキ名が51文字以上の場合に無効であること" do
+      deck = build(:deck, name: "a" * 51)
+      expect(deck).to be_invalid
+      expect(deck.errors[:name]).to include('は50文字以内で入力してください')
+    end
+
+    it "説明が101文字以上の場合に無効であること" do
+      deck = build(:deck, description: "a" * 101)
+      expect(deck).to be_invalid
+      expect(deck.errors[:description]).to include('は100文字以内で入力してください')
+    end
+
+    it "公開設定がnilの場合に無効であること" do
+      deck = build(:deck, public: nil)
+      expect(deck).to be_invalid
+      expect(deck.public).to be_nil
+      expect(deck.errors[:public]).to include("は一覧にありません")
+    end
+  end
 end

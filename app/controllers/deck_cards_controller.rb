c5a99ec540497e97e5fb12271525087feb48ba9d
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2025-12-29 11:39:26 +0900

    add:DeckCardsコントローラーの生成

---
+++ app/controllers/deck_cards_controller.rb
@@ -0,0 +1,4 @@
+class DeckCardsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "deck_cards/index"
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"

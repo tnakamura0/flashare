1ae32494701f5e8f58ffe60ceb430774e92b6d67
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2025-12-31 07:27:47 +0900

    add:answersテーブルのマイグレーションの適用

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_30_111425) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_30_215913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.boolean "correct"
+    t.bigint "card_id", null: false
+    t.bigint "quiz_session_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_id"], name: "index_answers_on_card_id"
+    t.index ["quiz_session_id"], name: "index_answers_on_quiz_session_id"
+  end
 
   create_table "cards", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +73,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_30_111425) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "cards"
+  add_foreign_key "answers", "quiz_sessions"
   add_foreign_key "cards", "users"
   add_foreign_key "deck_cards", "cards"
   add_foreign_key "deck_cards", "decks"

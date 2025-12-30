936102fc60f3e4224939c13292c01eb3f8ea3958
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2025-12-30 20:23:40 +0900

    update:マイグレーションの適用

--- db/migrate/20251230111425_create_quiz_sessions.rb
+++ db/migrate/20251230111425_create_quiz_sessions.rb
@@ -1,10 +1,10 @@
 class CreateQuizSessions < ActiveRecord::Migration[7.2]
   def change
     create_table :quiz_sessions do |t|
-      t.integer :current_question_index
+      t.integer :current_question_index, default: 0
       t.integer :total_questions
-      t.integer :status
-      t.references :user, null: false, foreign_key: true
+      t.integer :status, default: 0, null: false
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
       t.references :deck, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_29_021645) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_30_111425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_29_021645) do
     t.index ["user_id"], name: "index_decks_on_user_id"
   end
 
+  create_table "quiz_sessions", force: :cascade do |t|
+    t.integer "current_question_index", default: 0
+    t.integer "total_questions"
+    t.integer "status", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.bigint "deck_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deck_id"], name: "index_quiz_sessions_on_deck_id"
+    t.index ["user_id"], name: "index_quiz_sessions_on_user_id", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -55,4 +67,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_29_021645) do
   add_foreign_key "deck_cards", "cards"
   add_foreign_key "deck_cards", "decks"
   add_foreign_key "decks", "users"
+  add_foreign_key "quiz_sessions", "decks"
+  add_foreign_key "quiz_sessions", "users"
 end

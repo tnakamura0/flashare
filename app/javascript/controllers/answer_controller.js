1da43fc68a72f1b202470c545b8c9d7e8b49ad86
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-01-02 11:04:57 +0900

    add:stimulusにより解答表示を切り替える機能の実装

--- app/javascript/controllers/answer_controller.js
+++ app/javascript/controllers/answer_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="switch-answer-button"
 export default class extends Controller {
-  connect() {
+  static targets = [ "showAnswer", "answerButton" ]
+
+  switchButton() {
+    this.showAnswerTarget.classList.toggle("hidden")
+    this.answerButtonTarget.classList.toggle("hidden")
   }
 }

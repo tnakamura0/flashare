ad5a637d70dd0516f108566dc17f731c0767428a
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-01-03 10:15:34 +0900

    update:他のユーザーが公開しているデッキ表示する機能&そのデッキをクイズに追加する機能の実装

--- app/controllers/public_decks_controller.rb
+++ app/controllers/public_decks_controller.rb
@@ -1,4 +1,6 @@
 class PublicDecksController < ApplicationController
   def index
+    other_person_decks = Deck.where.not(user_id: session[:user_id])
+    @public_decks = other_person_decks.where(public: true)
   end
 end

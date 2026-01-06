f36ae79d5e569ab6291964781e8440ef2ef8e7d1
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-01-06 21:57:02 +0900

    update:アプリ全体のデザインを更新

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hamburger"
 export default class extends Controller {
-  static targets = [ "hamburgerLine1", "hamburgerLine2", "hamburgerLine3", "hamburgerMenu", "overlay" ]
+  static targets = [ "hamburgerLine1", "hamburgerLine2", "hamburgerLine3", "hamburgerMenuMobile", "hamburgerMenu", "overlay" ]
 
   switchMenu() {
     this.hamburgerLine1Target.classList.toggle("top-4")
@@ -15,7 +15,10 @@ export default class extends Controller {
     this.hamburgerLine3Target.classList.toggle("top-6")
     this.hamburgerLine3Target.classList.toggle("rotate-45")
 
-    this.hamburgerMenuTarget.classList.toggle("-right-4/5")
+    this.hamburgerMenuMobileTarget.classList.toggle("-right-4/5")
+    this.hamburgerMenuMobileTarget.classList.toggle("right-0")
+
+    this.hamburgerMenuTarget.classList.toggle("-right-3/10")
     this.hamburgerMenuTarget.classList.toggle("right-0")
 
     this.overlayTarget.classList.toggle("hidden")

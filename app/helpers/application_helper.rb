d32f63924d281b08b48f04ce91833df206a0cd4f
t.nakamura <206074491+tnakamura0@users.noreply.github.com>
2026-01-10 11:49:41 +0900

    add:OGP設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Flashare',
+      title: 'フラッシュカードを作成してシェアできるサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Flashareはカードを作成・解答して、作成したデッキを任意で公開できるフラッシュカードアプリです。',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@karume_Ao',
+      }
+    }
+  end
 end
